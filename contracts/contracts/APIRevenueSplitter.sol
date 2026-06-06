44560db3b98024fca4c377cbf0e5e3cd2a498e6c
Dark-Brain07 <Dark-Brain07@users.noreply.github.com>
2026-06-06 07:07:21 +0600

    feat(contracts): set creator and activate new API endpoint

--- contracts/contracts/APIRevenueSplitter.sol
+++ contracts/contracts/APIRevenueSplitter.sol
@@ -26,3 +26,5 @@ contract APIRevenueSplitter is Ownable {
     function registerApi(string memory endpointId) external {
         require(apiEndpoints[endpointId].creator == address(0), "Endpoint already registered");
         apiEndpoints[endpointId] = ApiEndpoint({
+            creator: msg.sender,
+            isActive: true,
