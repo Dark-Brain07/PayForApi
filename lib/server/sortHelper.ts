b8bdf888db7782248d8c254b39f510645f017e3f
Dark-Brain07 <Dark-Brain07@users.noreply.github.com>
2026-06-13 06:06:31 +0600

    types(sortHelper): replace any with generic array type

--- lib/server/sortHelper.ts
+++ lib/server/sortHelper.ts
@@ -5,7 +5,7 @@ export class SortHelper {
     if (!SortHelper.instance) SortHelper.instance = new SortHelper();
     return SortHelper.instance;
   }
-  public sort(data: any[], key: string, order: 'asc' | 'desc' = 'asc') {
+  public sort<T>(data: T[], key: keyof T, order: 'asc' | 'desc' = 'asc') {
     return [...data].sort((a, b) => {
       if (a[key] < b[key]) return order === 'asc' ? -1 : 1;
       if (a[key] > b[key]) return order === 'asc' ? 1 : -1;
