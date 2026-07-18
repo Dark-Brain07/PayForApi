8bf8e5780af6e86f3292ff7721510adb904c89e9
Dark-Brain07 <Dark-Brain07@users.noreply.github.com>
2026-07-18 08:16:49 +0600

    dx(wagmi): specify missing NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID fallback correctly in wagmiConfig.ts

--- lib/wagmiConfig.ts
+++ lib/wagmiConfig.ts
@@ -3,7 +3,7 @@ import { celo } from "viem/chains";
 
 export const wagmiConfig = getDefaultConfig({
   appName: "Pay For API",
-  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID || "",
+  projectId: process.env.NEXT_PUBLIC_WALLETCONNECT_PROJECT_ID ?? "",
   chains: [celo],
   ssr: true,
 });
