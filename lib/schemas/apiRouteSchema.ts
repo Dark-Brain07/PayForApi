e7beec1b6d7896c2d4b6fc770a4f4eb8a2e62dcb
Dark-Brain07 <Dark-Brain07@users.noreply.github.com>
2026-07-22 06:26:25 +0600

    types(schemas): add max constraint to string field

--- lib/schemas/apiRouteSchema.ts
+++ lib/schemas/apiRouteSchema.ts
@@ -4,5 +4,5 @@ export const apiRouteSchema = z.object({
   id: z.string().uuid(),
   path: z.string().startsWith("/").max(256),
   method: z.enum(["GET", "POST", "PUT", "DELETE", "PATCH"]),
-  description: z.string().trim().optional()
+  description: z.string().trim().max(256).optional()
 });
