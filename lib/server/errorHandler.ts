c230b183f9e56ffecc1b1f4204d1de63b00e2b1b
Dark-Brain07 <Dark-Brain07@users.noreply.github.com>
2026-07-22 07:28:40 +0600

    types(server): change handle error parameter from Error to unknown in errorHandler.ts

--- lib/server/errorHandler.ts
+++ lib/server/errorHandler.ts
@@ -3,7 +3,6 @@ import { loggerInstance } from "./logger";
 export class ErrorHandler {
   private static instance: ErrorHandler;
   private constructor() {}
-  /** Returns the singleton ErrorHandler instance */
   public static getInstance(): ErrorHandler {
     if (!ErrorHandler.instance) ErrorHandler.instance = new ErrorHandler();
     return ErrorHandler.instance;
@@ -11,8 +10,9 @@ export class ErrorHandler {
   /**
    * Handles server errors and returns a standardized response object.
    */
-  public handle(error: Error): { statusCode: number, message: string } {
-    loggerInstance.error('[ErrorHandled]', error.message);
+  public handle(error: unknown): { statusCode: number, message: string } {
+    const msg = error instanceof Error ? error.message : String(error);
+    loggerInstance.error('[ErrorHandled]', msg);
     return { statusCode: 500, message: 'Internal Server Error' };
   }
 }
